40bc3b98a348dc79741c5ffb526e99445a01d2fc
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-08 17:27:51 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - the main block
- * Return 0
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
