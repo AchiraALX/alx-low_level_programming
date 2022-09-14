9583486b3fd5a0ecc624cc6c0849c2e2a6e86d42
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-14 21:48:04 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,23 +3,22 @@
  * print_alphabet_x10 - Print the letters in lowercase 10 time
  * Return: Always 0 (Success)
  */
-
 void print_alphabet_x10(void)
 {
       int i = 0;
       char c;
-  
+
       while (i < 10)
       {
             c = 'a';
-            
+
             while (c <= 'z')
             {
-                _putchar(c);
-                c++;
-            }
-        
-            _putchar('\n');
-            i++;
+            _putchar(c);
+            c++;
+      }
+
+      _putchar('\n');
+      i++;
       }
 }
