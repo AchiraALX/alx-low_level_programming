a596182355e8b99abe5e802b25c65b177e19cb70
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-15 12:14:06 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,44 @@
-#include "main.h"
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * times_table - Prints 9 time table starting with 0
- * Return: Always 0 (Success)
+ * times_table - Print the 9 times table, starting with 0
+ * Return: 0
  */
-
 void times_table(void)
 {
-	for (int y = 0; y < 10; y++)
+	int x = 0; /* factor */
+	int y; /* count  */
+	int z; /* computed value */
+
+	while (x < 10)
 	{
-		for (int z = 0; z < 10; z++)
-			z *= y;
-			_putchar(z / 10 + '0');
-			_putchar(z % 10 + '0');
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
 		}
-		_putchar('\n')
+		_putchar('\n');
+		x++;
 	}
 }
