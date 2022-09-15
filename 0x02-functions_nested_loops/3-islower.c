376cc2397bfff6e588562e583618d5de4ca48d3c
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-15 09:30:05 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,13 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _islower - Prints 1 if the character is lowercase and 0 if otherwise
- * Return: Always 0 (Success)
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		_putchar('1');
+	if (c > 96 && c < 123)
+		return (1);
 	else
-		_putchar('0');
+		return (0);
 }
