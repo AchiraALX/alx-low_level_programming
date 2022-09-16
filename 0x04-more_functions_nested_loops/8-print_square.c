af44fc0c14db8cd7c66f043cee8a7a29e6ac797f
AchiraALX <ed127720@students.mu.ac.ke>
2022-09-16 01:08:29 -0700

    Making a square program

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - stacks '#' to make a square of a given size
+ * @size: type int number
+ * Return:Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int x;
+
+	x = 0;
+	while (x <= size)
+	{
+		y = 0;
+		while (y <= size)
+		{
+			_putchar('#');
+			y++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+}
