206a5992ff8230b421ed0e5c45ba5f101b5d41d8
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-19 14:03:38 +0300

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: char array string type
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i, c, l;
+	char h;
+
+	/* gets each character in a string */
+  for (i = 0; s[i] != '\0'; i++)
+		;
+
+	l = i;
+  /* carries the reversing process */
+	for (i--, c = 0; c < l / 2; i--, c++)
+	{
+		/* h temporarily stores the character on the process */
+    h = s[c];
+		s[c] = s[i];
+		s[i] = h;
+	}
+}
