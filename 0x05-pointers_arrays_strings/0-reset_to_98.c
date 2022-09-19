65b29b5d5fa3a6553e5d2af84006626f42fc3adc
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-19 13:32:39 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Sets pointer parameter to 98.
+ * @n: int type pointer
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
