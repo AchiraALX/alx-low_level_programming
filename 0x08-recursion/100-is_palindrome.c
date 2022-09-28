4ae0cb8cca086f5eddf80ca6c561845d8a005109
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-28 21:46:03 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,3 +37,18 @@ int is_palindrome_helper(char *s, int i, int j)
 		return (0);
 	return (is_palindrome_helper(s, i + 1, j - 1));
 }
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string to check
+ *
+ * Return: length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s +1));
+}
