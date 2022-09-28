6ebc0b2521ae3528b3de20bc2b7b8f7d80fca29b
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-09-28 21:36:29 +0300

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a srring is palindrome
+ *
+ * @s: string to check
+ *
+ * Return: 1 if the string is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	
+	if (len <= 1)
+		return (1);
+	return (is_palindrome_helper(s, 0, len - 1));
+}
+
+/**
+ * is_palindrome_helper - the helper function for is_palindrome
+ *
+ * @s: string to check
+ *
+ * @i: index of the first character
+ *
+ * @j: index of the last character
+ *
+ * Return: 1 if is a palindrome, 0 otherwise
+ */
+
+int is_palindrome_helper(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+	if (s[i] != s[j])
+		return (0);
+	return (is_palindrome_helper(s, i + 1, j - 1));
+}
