3c4487a15da4905e48839189c9175fb324c40d1a
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-10-04 14:17:02 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		reutrn (NULL);
+
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
+
 	return (array);
 }
