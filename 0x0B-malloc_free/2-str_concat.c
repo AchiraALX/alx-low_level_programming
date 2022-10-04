e8230816784f7869b85ed81658b019da580651b1
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-10-04 15:28:52 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  *
  * @s2: string 2
  *
- * Return pointer to the new string
+ * Return: pointer to the new string
  */
 
 char *str_concat(char *s1, char *s2)
@@ -15,7 +15,6 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, j, len1, len2;
 
-	/* if either s1 or s2 is NULL, treat it as an empty string */
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -26,7 +25,6 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	/* find the length of s1 and s2 */
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 		;
@@ -37,28 +35,23 @@ char *str_concat(char *s1, char *s2)
 		;
 	}
 
-	/* allocate memory for the new string */
 	s = malloc((len1 + len2 + 1) * sizeof(*s));
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 
-	/* copy s1 to s */
 	for (i = 0; i < len1; i++)
 	{
 		s[i] = s1[i];
 	}
 
-	/* copy s2 to s*/
 	for (j = 0; j < len2; j++)
 	{
 		s[i + j] = s2[j];
 	}
 
-	/* add the terminating null byte */
 	s[i + j] = '\0';
 
-	/* return the pointer to the new string */
 	return (s);
 }
