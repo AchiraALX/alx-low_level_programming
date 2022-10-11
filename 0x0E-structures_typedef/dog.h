d1456738fc4b167eb9f824c45d67f4e081444874
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-10-11 15:21:56 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,11 @@
 #define DOG_H
 
 /**
- * struct dog - dog
- *
- * @name: name
- *
- * @age: age
- *
- * @owner: owner
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
-
 typedef struct dog
 {
 	char *name;
@@ -20,8 +16,6 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /* DOG_H */
+#endif
