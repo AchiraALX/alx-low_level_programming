0d6d652c212967569edb05df5f875692cfefd52c
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2022-10-13 14:06:59 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,28 +13,28 @@
 
 int main(int argc, char *argv[])
 {
-    int i, n;
+	int i, n;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    n = atoi(argv[1]);
-    if (n < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%02hhx", ((char *)main)[i]);
-        if (i < n - 1)
-            printf(" ");
-    }
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
 
-    printf("\n");
-    return (0);
+	printf("\n");
+	return (0);
 }
