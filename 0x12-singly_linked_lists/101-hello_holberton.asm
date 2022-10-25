dbe3d2e3542de46047131431b06dbb651b3b809c
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-10-26 00:04:26 +0300

    Hit a wall once on assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-section	.text
-   global _start     ;must be declared for linker (ld)
-	
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
-   int	0x80        ;call kernel
-	
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+global_start
 
-section	.data
-msg db 'Hello, Holberton!', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
+section .text
+
+_start:
+	mov rax, 1	;	write(
+	mov rdi, 1	;	STDOUT_FILENO,
+	mov rsi, msg	;	"Hello, Holberton\n",
+	mov rdx, msglen	;	sizeof("Hello, Holberton\n")
+	syscall		; );
