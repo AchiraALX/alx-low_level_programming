0debaafb9b8ce7c1f985abc8a59dc8dcb6ccd779
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-10-26 18:55:21 +0300

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets
+ * head to NULL
+ *
+ * @head: pointer to the list
+ *
+ * Return: void
+*/
+
+void free_listint2(const listint_t *h)
