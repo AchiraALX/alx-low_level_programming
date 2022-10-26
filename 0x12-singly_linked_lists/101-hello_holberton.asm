f12f50a4225d78c67bf2eb86695836825cb6d4b3
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-10-26 09:51:41 +0300

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,11 +3,11 @@ global_start
 section .text
 
 _start:
-	mov rax, 1	;	write(
-	mov rdi, 1	;	STDOUT_FILENO,
-	mov rsi, msg	;	"Hello, Holberton\n",
-	mov rdx, msglen	;	sizeof("Hello, Holberton\n")
-	syscall		; );
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
 
 section .data
 msg:	db "Hello, Holberton\n"
