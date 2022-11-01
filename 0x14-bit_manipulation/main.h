3e8a7dd251cd98173ddaafbde60835d46ffdd0b8
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-11-01 04:41:59 +0300

    Create mainn.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define BIT(x) (1 << (x))
+#define BIT_SET(x, y) ((x) |= (y))
+#define BIT_CLEAR(x, y) ((x) &= ~(y))
+#define BIT_FLIP(x, y) ((x) ^= (y))
+#define BIT_CHECK(x, y) ((x) & (y))
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+
+#endif
