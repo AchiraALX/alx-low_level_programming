afa7745b8bc561f5e3d86d5eb0a40296b5bf9560
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-11-03 04:53:43 +0300

    Create 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,24 +1,39 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - create the copy bash script
  *
- * Return: Always 0.
- */
+ * @ac: argument count
+ *
+ * @av: argument vector
+ *
+ * Return: 0 Success
+*/
 
-int main(void)
+int main(int ac, char **av)
 {
-    int fd;
-    char *text_content = "Holberton School!\n";
-    int res;
+	int fd, fd2, read_bytes, write_bytes;
+	char buffer[1024];
 
-    fd = open("file", O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    close(fd);
-    res = append_text_to_file("file", text_content);
-    printf("-> %i)\n", res);
-    res = append_text_to_file("file", NULL);
-    printf("-> %i)\n", res);
-    res = append_text_to_file(NULL, text_content);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((read_bytes = read(fd, buffer, 1024)) > 0)
+	{
+		write_bytes = write(fd2, buffer, read_bytes);
+		if (write_bytes == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
+	if (read_bytes == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+	return (0);
 }
