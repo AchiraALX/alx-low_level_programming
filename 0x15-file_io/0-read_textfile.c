1a1c126af729b21a10cbd8cd815dcc0609965575
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-11-03 04:23:01 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints
+ * it to POSIX standard output
+ *
+ * @filename: filename of the file to be read
+ *
+ * @letters: letter to be read
+ *
+ * Return: ssize_t
+*/
+
+ssize_t read_textfile(const char filename, size_t letters)
+{
+        int fd;
+        ssize_t read_bytes, write_bytes;
+        char *buffer;
+
+        if (filename == NULL)
+                return (0);
+
+        buffer = malloc(sizeof(char) * letters);
+        if (buffer == NULL)
+                return (0);
+
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+        {
+                free(buffer);
+                return (0);
+        }
+
+        read_bytes = read(fd, buffer, letters);
+        if (read_bytes == -1)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
+
+        write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+        if (write_bytes == -1 || write_bytes != read_bytes)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
+
+        free(buffer);
+        close(fd);
+        return (write_bytes);
+}
